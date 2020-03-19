fc6d46bc5ba25295f0bb0d78c6f9db146d0fedf7
Keanu Williams <keanucwilliams@gmail.com>
2020-03-19 12:05:03 -1000

    Update gitignore and MAIL_URL

--- app/server/main.js
+++ app/server/main.js
@@ -4,3 +4,5 @@ import '/imports/startup/server/Mongo';
 import '/imports/startup/server/Publications';
 import '/imports/startup/server/Profiles';
 import '/imports/startup/server/methods/sendVerificationEmail.js';
+
+process.env.MAIL_URL = Meteor.settings.private.env.MAIL_URL;
