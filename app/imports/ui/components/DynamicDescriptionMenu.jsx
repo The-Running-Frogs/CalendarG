4a228e72c10a96675c9228be466b512e7d016566
JeffSuen <jsuen98@gmail.com>
2020-04-26 14:16:15 -1000

    Landing Page text additions
    Added in text box and visual elements in the landing page. Still has filler in them in case we needed anything else

--- app/imports/ui/components/DynamicDescriptionMenu.jsx
+++ app/imports/ui/components/DynamicDescriptionMenu.jsx
@@ -3,24 +3,26 @@ import { Grid, Segment, Menu } from 'semantic-ui-react'
 
 export default class DynamicDescriptionMenu extends Component {
 
-    state = { activeItem: 'bio' }
+    state = { activeItem: 'What is this?' }
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
     render() {
+        const square = {width: 100, height: 100}
         const { activeItem } = this.state
         return (
+
             <Grid>
                 <Grid.Column width={4}>
                     <Menu fluid vertical tabular pointing>
                         <Menu.Item
-                            name = 'bio'
-                            active={activeItem === 'bio'}
+                            name = 'What is this?'
+                            active={activeItem === 'What is this?'}
                             onClick={this.handleItemClick}
                         />
                         <Menu.Item
-                            name = 'about'
-                            active={activeItem === 'about'}
+                            name = 'Get Started'
+                            active={activeItem === 'Get Started'}
                             onClick={this.handleItemClick}
                         />
                         <Menu.Item
@@ -36,7 +38,7 @@ export default class DynamicDescriptionMenu extends Component {
                     </Menu>
                 </Grid.Column>
                 <Grid.Column stretched width={12}>
-                   <RenderedContent tabName={activeItem} />
+                   <RenderedContent tabName={activeItem} format={square}/>
                 </Grid.Column>
             </Grid>
         )
@@ -44,30 +46,49 @@ export default class DynamicDescriptionMenu extends Component {
 }
 
 
-const RenderedContent = ({ tabName }) => {
-    if (tabName === 'bio') {
-        return <Bio/>
+const RenderedContent = ({ tabName, format }) => {
+    if (tabName === 'What is this?') {
+        return <WhatIsThis format={format}/>
     }
-    if (tabName === 'about') {
-        return <About/>
+    if (tabName === 'Get Started') {
+        return <GetStarted/>
     }
     return <Default/>
 };
 
-const Bio = () => (
-    <Segment>
-        BIO This is an stretched grid column. This segment will always match the tab height
+const WhatIsThis = ({format}) => (
+
+    <Segment centered raised padded>
+
+        <p>CalendarG is a web tool developed by The Running Frogs to help its users stay organized and on top of things with an online Calendar interface. Every user gets their own account that they can use to establish a calendar and schedule for themselves.</p>
+        <p>The interface will be something familiar to those who have used services such as Google Calendars before and aims to be simple enough for intuitive use to those less familiar with those types of products as well!</p>
+
+        <Grid padded centered columns={1}>
+            <Grid.Row centered columns={3}>
+                <Grid.Column>
+                    <Segment color='teal' inverted circular style={format}>Easy</Segment>
+                </Grid.Column>
+                <Grid.Column>
+                    <Segment color='teal' inverted circular style={format}>Tidy</Segment>
+                </Grid.Column>
+                <Grid.Column>
+                    <Segment color='teal' inverted circular style={format}>Fun</Segment>
+                </Grid.Column>
+             </Grid.Row>
+        </Grid>
     </Segment>
 );
 
-const About = () => (
-    <Segment>
-        ABOUT This is an stretched grid column. This segment will always match the tab height
+const GetStarted = () => (
+
+    <Segment raised>
+        <p>To get started, please click on 'Don't have an account?' in the top right corner of this page! Once you've completed the form please check your email to verify your account!</p>
     </Segment>
+
 );
 
 const Default = () => (
-    <Segment>
+    <Segment raised>
         Default message, please implement the proper formatting
     </Segment>
 );
